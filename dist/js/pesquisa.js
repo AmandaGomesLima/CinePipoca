b674d1032a7387927791944b90f42bcb269e2aab
Amanda Lima <aglima.amanda@gmail.com>
2021-06-19 00:09:11 -0300

    [amanda] Atualizaçao da pagina Pesquisa

--- dist/js/pesquisa.js
+++ dist/js/pesquisa.js
@@ -3,12 +3,19 @@ function setLista(responseText) {
   var $filmeDetalhe = $('.pesquisa-lista');
   
   $.each(json.results, function(index, value) {
+    let imagem;
+    
+    if (value.poster_path) {
+      imagem = '<a href="https://www.themoviedb.org/movie/' + value.id + '">' +
+                  '<img class="img-fluid" src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2' + value.poster_path + '" alt="' + value.title + '">' +
+               '</a>';
+    }
+    else {
+      imagem = '<div class="glyphicons"></div>';
+    }
+
     let item = '<div class="row">' +
-                  '<div class="col-12 col-md-2 d-sm-none d-md-block">' +
-                    '<a href="https://www.themoviedb.org/movie/' + value.id + '">' +
-                      '<img class="img-fluid" src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2' + value.poster_path + '" alt="' + value.title + '">' +
-                    '</a>' +
-                  '</div>' +
+                  '<div class="col-12 col-md-2 d-none d-md-block">' + imagem + '</div>' +
                   '<div class="col-12 col-md-10">' +
                     '<h3 class="titulo text-warning">' + 
                       '<a href="https://www.themoviedb.org/movie/' + value.id + '">' + value.title + '</a>' +
@@ -25,5 +32,5 @@ $(function() {
   var queryString = getUrlVars();
   theMovieDb.search.getMovie({'language': 'pt-BR', 'query': queryString.p}, setLista, setError);
 
-  $().html(queryString.p);
+  $('#filme-pesquisa .termo span').html(queryString.p);
 });
